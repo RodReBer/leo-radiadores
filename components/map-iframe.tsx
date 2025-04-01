af3a6bfcf0c46638dad7e8824f018852011587a2
RodReBer <rodrigorey2005@gmail.com>
2025-04-01 13:54:18 -0300

    maps

--- components/map-iframe.tsx
+++ components/map-iframe.tsx
@@ -9,28 +9,28 @@ const locations = [
     name: "Sede 1 - General Flores",
     address: "General Flores 4270 esq. Londres, Montevideo",
     embedUrl:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3273.8481326125384!2d-56.16023492330906!3d-34.83124597304244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a02ab071e7e2c1%3A0x9e0d1b6a3d6a1e0!2sGeneral%20Flores%204270%2C%2012000%20Montevideo%2C%20Uruguay!5e0!3m2!1ses!2sus!4v1711842000000!5m2!1ses!2sus",
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6548.314838637013!2d-56.162498924009306!3d-34.85227167286617!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a02a80cfaffff3%3A0xa7aca2ffb4700642!2sLeo%20Radiadores!5e0!3m2!1ses!2sus!4v1743526414871!5m2!1ses!2sus",
   },
   {
     id: 2,
     name: "Sede 2 - Galicia",
     address: "Galicia 1286 esq. Yaguarón, Montevideo",
     embedUrl:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3271.9307079282574!2d-56.19384992330636!3d-34.90683797304244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f81c1b9d092a5%3A0x7c4a9f7c5d5c5d5c!2sGalicia%201286%2C%2011100%20Montevideo%2C%20Uruguay!5e0!3m2!1ses!2sus!4v1711842000000!5m2!1ses!2sus",
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3272.25104847476!2d-56.19155972400726!3d-34.900148872849535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f8117b9e790ab%3A0xb25ddfdb5994b031!2sLeo%20Radiadores!5e0!3m2!1ses!2sus!4v1743526387530!5m2!1ses!2sus",
   },
   {
     id: 3,
     name: "Sede 3 - Ejido",
     address: "Ejido 1648 esq. Galicia, Montevideo",
     embedUrl:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3271.9307079282574!2d-56.18704992330636!3d-34.90953797304244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f81c1b9d092a5%3A0x7c4a9f7c5d5c5d5c!2sEjido%201648%2C%2011100%20Montevideo%2C%20Uruguay!5e0!3m2!1ses!2sus!4v1711842000000!5m2!1ses!2sus",
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3272.253172040914!2d-56.18992632400738!3d-34.900095572849544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f8033fb452967%3A0x60f39a37d20829f6!2sLeo%20Radiadores!5e0!3m2!1ses!2sus!4v1743526317666!5m2!1ses!2sus",
   },
   {
     id: 4,
     name: "Sede 4 - Giannattasio",
     address: "Av. Giannattasio km 21400, Ciudad de la Costa",
     embedUrl:
-      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3273.8481326125384!2d-55.95804992330906!3d-34.82354597304244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a02ab071e7e2c1%3A0x9e0d1b6a3d6a1e0!2sAv.%20Giannattasio%20Km%2021.400%2C%20Ciudad%20de%20la%20Costa%2C%20Uruguay!5e0!3m2!1ses!2sus!4v1711842000000!5m2!1ses!2sus",
+      "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6549.616919582815!2d-55.98720302400994!3d-34.835908072871895!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x959f89f78b206bef%3A0x18b66be361ae8b55!2sLeo%20Radiadores!5e0!3m2!1ses!2sus!4v1743526205020!5m2!1ses!2sus",
   },
 ]
 
